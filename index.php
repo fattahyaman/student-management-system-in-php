881115f0547794c15c55c991022b5a65c06142c0
yaman <maniyaman76@gmail.com>
2019-07-18 01:01:23 +0200

    new updates

--- index.php
+++ index.php
@@ -9,65 +9,48 @@
 	<h3 align="right"><a href="login.php">Admin login</a></h3>
 
 	<form method="post" action="index.php">
-		 
-	
+		<table style="width: 50%;" align="center">
+			<tr>
+				<td colspan="2" align="center"><h1>Student Information</h1></td>
+			</tr>
 
+			<tr>
+				<td>Choose Standad</td>
+				<td>
+					<select name="std">
+						<option value="1">1st</option>
+						<option value="2">2st</option>
+						<option value="3">3st</option>
+						<option value="4">4st</option>
+						<option value="5">5st</option>
+						<option value="6">6st</option>
+					</select>
+				</td>
+			</tr>
 
-	<table style="width: 50%;" align="center">
-		<tr>
-			<td colspan="2" align="center"><h1>Student Information</h1></td>
-		</tr>
+			<tr>
+				<td>Enter Roll No</td>
+				<td>
+					<input type="text"  name="rollno" required>
+				</td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit" class="button" name="submit" value="Show Details">
+				</td>
+			</tr>
 
-		<tr>
-			<td>Choose Standad</td>
-			<td>
-				<select name="std">
-					<option value="1">1st</option>
-					<option value="2">2st</option>
-					<option value="3">3st</option>
-					<option value="4">4st</option>
-					<option value="5">5st</option>
-					<option value="6">6st</option>
-
-				</select>
-			</td>
-		</tr>
-
-		<tr>
-			<td>Enter Roll No</td>
-			<td>
-				<input type="text"  name="rollno" required>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="2"><input type="submit" class="button" name="submit" value="Show Details">
-			</td>
-		</tr>
-	</table>
-</form>
-	
+		</table>
+	</form>
 </body>
 </html>
 
 <?php
-
-
 if(isset($_POST['submit']))
 {
-	
-
 	$standard = $_POST['std'] ;
 	$rollno = $_POST['rollno'] ;
-
 	include('dbcon.php');
 	include('showdetails.php');
-
-
 	showdetails($standard,$rollno);
-	
-
-
 	}
-
-
 ?>
